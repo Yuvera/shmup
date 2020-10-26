5ac832a091dc26414bbc4428c6cd8ef88bf26a9f
Yuvera <73469427+Yuvera@users.noreply.github.com>
2020-10-26 10:34:05 +0000

    Initial commit

---
+++ shmup/Game1.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace shmup
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D saucerTxr, missileTxr, backgroundTxr;
+        Point screenSize = new Point(800, 450);
+
+        Sprite backgroundSprite;
+        PlayerSprite PlayerSprite;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+
+            _graphics.PreferredBackBufferWidth = screenSize.X;
+            _graphics.PreferredBackBufferHeight = screenSize.Y;
+            _graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            saucerTxr = Content.Load<Texture2D>("saucer");
+            missileTxr = Content.Load<Texture2D>("missile");
+            backgroundTxr = Content.Load<Texture2D>("background");
+
+            backgroundSprite = new Sprite(backgroundTxr, new Vector2(0, 0));
+            PlayerSprite = new PlayerSprite(saucerTxr, new Vector2(screenSize.X/6, screenSize.Y/2));
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            PlayerSprite.Update(gameTime, screenSize);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            backgroundSprite.Draw(_spriteBatch);
+            PlayerSprite.Draw(_spriteBatch);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ shmup/Sprite.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace shmup
+{
+    public class Sprite
+    {
+        public Texture2D spriteTexture;
+        public Vector2 spritePos;
+
+    public Sprite(Texture2D newTxr, Vector2 newPos)
+        {
+            spriteTexture = newTxr;
+            spritePos = newPos;
+        }
+
+        public virtual void Update(GameTime gameTime, Point screenSize) { }
+
+        public void Draw(SpriteBatch _spriteBatch)
+        {
+            _spriteBatch.Draw(spriteTexture, new Rectangle(
+                (int)spritePos.X,
+                (int)spritePos.Y,
+                spriteTexture.Width,
+                spriteTexture.Height),
+                Color.White
+                );
+        }
+
+    }
+}
